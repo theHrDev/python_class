d3a87dcd55fa3a3d5b9a4d7a07dafcb1a2cda18f
theHrDev <hamzatrukayat2822@gmail.com>
2025-10-31 22:26:55 +0100

    while loop exercise

---
+++ whileloop.py
@@ -0,0 +1,24 @@
+# while true:
+#     print("True")
+
+
+i = 10
+
+while i <= 20:
+    print(i)
+    i +=1
+
+
+a = 20
+
+while a > 0:
+    print(a)
+    a-=1
+
+
+starting = int(input("Enter the starting point: "))
+ending = int(input("Enter the ending point: "))
+
+while starting <= ending:
+    print(starting)
+    starting += 1
