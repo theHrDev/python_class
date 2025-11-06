8d229fd5d2058c2feeef1328021175003ab107e0
theHrDev <hamzatrukayat2822@gmail.com>
2025-11-06 07:26:04 +0100

    Added how break and continue works in while loop

---
+++ loop.py
@@ -0,0 +1,33 @@
+
+
+# while num != "10":
+#     print("Wrong")
+#     num = input("Guess another number: ")
+# print("Correct")
+
+# break
+
+
+# BREAK - for terminating
+i = 4
+while i < 10:
+    print(i)
+    if(i == 7):
+        break
+    i = i + 1
+5, 6, 7, 9
+
+
+# CONTINUE - for skipping
+
+i = 5
+while i < 10:
+    print("Hello")
+    if i == 8:
+        i = i + 1
+        continue
+    print(i)
+    i = i + 1
+
+
+    
