24de8a7b97db5b7cba95e7e4ac7e4d40a92fd62f
theHrDev <hamzatrukayat2822@gmail.com>
2025-11-09 20:26:21 +0100

    for loop

---
+++ forloop.py
@@ -0,0 +1,20 @@
+# names = ["Taye", "Kehinde", "Idowu", "Rukayat", "Taiwo"]
+# for name in names:
+#     print("val")
+#     print(name)
+
+
+# print(f"My name is {names[0]}")
+# print(f"My name is {names[1]}")
+# print(f"My name is {names[2]}")
+# print(f"My name is {names[3]}")
+# print(f"My name is {names[4]}")
+# nums = [8, 12, 15, 6, 20, 6, 56, 1]
+# total = max(nums)
+# print(total)
+# total = 0
+# for num in nums:
+#     total = total + num
+#     print(total) 
+
+# print(f"The sum of all the values in the list is {total}")
