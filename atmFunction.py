3f894d45756eb0e6955abdb167cb425a3d1ded11
theHrDev <hamzatrukayat2822@gmail.com>
2025-11-21 12:25:58 +0100

    Added the transaction history to the atm simulation function

--- atmFunction.py
+++ atmFunction.py
@@ -1,18 +1,23 @@
 
 print("Welcome Back")
 total_balance = int(input("Enter your intial balance: "))
+transaction_history = {}
 
 def deposit():
-    global total_balance
+    global total_balance, transaction_history
     deposited_amount = int(input("Enter the amount you want to deposit: "))
     total_balance = total_balance + deposited_amount
+    transaction_message = f"You deposited {deposited_amount} into your account"
+    transaction_history["Deposit"] = [transaction_message]
     print("Amount Desposited successfully")
     print("Your balance is ")
     print(total_balance)
 
 def withdraw():
-    global total_balance
+    global total_balance, transaction_history
     withdrawn_amount = int(input("Enter the amount you want to withdraw: "))
+    transaction_message = f"{withdrawn_amount} was withdrawn from  your account"
+    transaction_history["Withdraw"] = [transaction_message]
     if(withdrawn_amount <= total_balance):
         total_balance = total_balance - withdrawn_amount
         print("Amount Withdrawn successfully")
@@ -29,15 +34,23 @@ def balance():
     print("Your balance is ")
     print(total_balance)
 
+def transaction_his():
+    print("Your transaction summary; ")
+    print(transaction_history)
+    
+    
+    
 def bank_transaction():
     print("What do you want to do")
-    transaction = input("1. Deposit  2.Withdraw  3.Check Balance:  " )
+    transaction = input("1. Deposit  2.Withdraw  3.Check Balance: 4. Transaction History " )
     if(transaction == "1"):
         deposit()
     elif(transaction == "2"):
         withdraw()
     elif(transaction == "3"):
         balance()
+    elif(transaction == "4"):
+        transaction_his()
     else:
         print("Unknown Transaction")
 
