fefbc60f878c7dffa5669172361a9cbff6ba6bd8
theHrDev <hamzatrukayat2822@gmail.com>
2025-11-22 22:06:20 +0100

    Added tuple- immutable data type

---
+++ tuple.py
@@ -0,0 +1,15 @@
+# names = ("Tolu", "Ade", "Aina")
+# names[0] = "Ola"
+# print(names)
+# student = {"name": "Taye", "dept": "CSC"}
+# for (x, y) in student.items():
+#     print(f"The key is {x} and the value is {y}")
+# names = ("Tolu", "Ade", "Aina", "Ade")
+
+# new_names = list(names)
+# new_names[0] = "Ola"
+# names = tuple(new_names)
+# print(names)
+
+# names = {"Tolu", "Ade", "Aina", "Tolu"}
+# print(names[0])
