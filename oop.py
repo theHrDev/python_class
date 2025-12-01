7d9bc3561dce18584179dbf9a2edc8cc88fb1d50
theHrDev <hamzatrukayat2822@gmail.com>
2025-12-01 08:32:40 +0100

    A well detailed oop beginner class

--- oop.py
+++ oop.py
@@ -17,4 +17,31 @@ person_1 = Person()
 print(person_1.name)
 print(person_1.greetUser())
 print(person_1.userInfo(3))
+
+
+
+# color = "red"
+# name = "Taye"
+# dept = "CSC"
+class Person:
+    def __init__(self, new_name, new_dept):
+        self.name = new_name
+        self.dept = new_dept
+        print(self.name, self.dept)
+        self.greet()
+
+
+    def say_hi(self):
+        print(f"{self.name} How are you doing today?")
+    
+    def change_name(self, new_name):
+        self.name = new_name
+    
+    def greet(self):
+        print("Good evening everyone")
+
+
+Person("Tolu", "BCH")
+person_2 = Person("Ajayi", "PSG")
+# print(person_1.name)
     
