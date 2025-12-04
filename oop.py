49451032bb1be46631a5dea7f6bbef6bbc53bf7d
theHrDev <hamzatrukayat2822@gmail.com>
2025-12-04 16:26:32 +0100

    Added class inheritance

--- oop.py
+++ oop.py
@@ -11,18 +11,18 @@ class Person:
         print(f"Hi {self.name}, you are in {self.dept} department and you are {age} years old")
     
     
-# Person() self in this case is referring to the object that is about to create
+Person()  #self in this case is referring to the object that is about to create
 person_1 = Person()
-# print(person_1)
+print(person_1)
 print(person_1.name)
 print(person_1.greetUser())
 print(person_1.userInfo(3))
 
 
 
-# color = "red"
-# name = "Taye"
-# dept = "CSC"
+color = "red"
+name = "Taye"
+dept = "CSC"
 class Person:
     def __init__(self, new_name, new_dept):
         self.name = new_name
@@ -41,7 +41,29 @@ class Person:
         print("Good evening everyone")
 
 
-Person("Tolu", "BCH")
-person_2 = Person("Ajayi", "PSG")
+# Person("Tolu", "BCH")
+# person_2 = Person("Ajayi", "PSG")
 # print(person_1.name)
+class Human():
+    head = 1
+    eyes = 2
+    ears = 2
+    def __init__(self,head,eyes,ears):
+        self.head = head
+        self.eyes = eyes
+        self.ears = ears
+        print(f"head {head} eyes {eyes} ears {ears}")
+    
+    
+
+class Man(Human):
+    voice = "deep"
+    def __init__(self, head, eyes, ears,voice):
+        self.voice = voice
+        super().__init__(head, eyes, ears)
+        print(voice)
+    
+    
+Man(2,4,5,"thin")
+# print(man_feature.ears)
     
