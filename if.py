5f0cbe418b060217f31728b4d7adc8affcfdc5ad
theHrDev <hamzatrukayat2822@gmail.com>
2026-01-13 21:53:04 +0100

    updated

--- if.py
+++ if.py
@@ -39,3 +39,73 @@ elif(username != repeated_username):
     print("Incorrect username")
 elif(password == repeated_password and username == repeated_username):
     print("Login Successful")
+    
+    
+#input("Enter your name: ")
+
+# age = int(input("Enter your age: "))
+
+# if( age > 18):
+#     print("you are eligible to vote")
+# else:
+#     print("you are not eligible to vote")
+    
+# print("Good")
+
+
+# if(age < 18):
+#     print("you are not eligible to vote")
+# else:
+#     print("you are eligible to vote")
+    
+# print("Good")
+
+# if(False):
+#     print("yes")
+# else:
+#     print("no")
+
+# num = 12345
+# print(type(num))
+
+# num_str = str(num)
+# print(num_str)
+# print(type(num_str))
+
+# num_bool = bool(num)
+# print(num_bool)
+# print(type(num_bool))
+
+# num_dec = float(num)
+# print(num_dec)
+# print(type(num_dec))
+
+# logical operator (and or)
+# num = 6  #assigning
+# if num < 6:
+#     print("num is lesser than 6")   
+# elif num == 4:
+#     print("num is equal to 4")
+# elif num >= 4:
+#     print("num is greater than or equal to 4")
+# else:
+#     print("num is greater than 6")
+    
+    
+# if num == 6:
+#     print("num is equal to 6")
+
+# Login system
+password = input("Enter your password: ")
+username = input("Enter your user name: ")
+print("Signup successfully, proceed to login")
+
+    
+repeat_username = input("Enter your username: ")
+repeat_password = input("Enter your password: ")
+if(repeat_password == password):
+    print("Login successfully")
+else:
+    print("Wrong password")
+    
+len("word ")
